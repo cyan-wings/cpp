f09f7ac5dcffdb68755cc12bff7ba7c5da2cdd6c
cyan-wings <matthewyeow.yeow@gmail.com>
2025-01-11 19:10:48 +0700

    Added print of data in cpp06 ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -7,6 +7,7 @@ int	main( void )
 	uintptr_t	raw = Serializer::serialize(&d);
 	Data		*deserializedPtr = Serializer::deserialize(raw);
 	
+	std::cout << "Initial data content: [" << d.getData() << "]" << std::endl;
 	std::cout << "Data deserialized: [" << deserializedPtr->getData() << "]" << std::endl;
 	return (0);
 }
